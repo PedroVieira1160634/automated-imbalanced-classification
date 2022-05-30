981dc5dd8ff0f67edfe814d797952e900609428d
PedroVieira <1160634@isep.ipp.pt>
2022-05-30 20:28:51 +0100

    .

--- project/_test.py
+++ project/_test.py
@@ -12,7 +12,8 @@ from lightgbm import LGBMClassifier
 from xgboost import XGBClassifier
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, make_scorer
+from imblearn.metrics import geometric_mean_score
 
 print('\n\n----------------------------------start -', datetime.now(), '--------------------------------------\n\n')
 
@@ -21,7 +22,7 @@ print('\n\n----------------------------------start -', datetime.now(), '--------
 #page-blocks0.dat
 #car-good.dat
 #kddcup-rootkit-imap_vs_back.dat #to be removed, only for tests
-dataset_name = "car-good.dat"
+dataset_name = "glass1.dat"
 df = pd.read_csv(sys.path[0] + "/input/" + dataset_name)
 
 X = df.iloc[: , :-1]
@@ -30,12 +31,12 @@ y = df.iloc[: , -1:]
 
 # -- characteristics of datasets
 
-df2 = df.iloc[: , :-1]
+# df2 = df.iloc[: , :-1]
 
-n_rows = len(df)
-n_columns = len(X.columns)
-n_numeric_col = X.select_dtypes(include=np.number).shape[1]
-n_non_numeric_col = X.select_dtypes(include=object).shape[1]
+# n_rows = len(df)
+# n_columns = len(X.columns)
+# n_numeric_col = X.select_dtypes(include=np.number).shape[1]
+# n_non_numeric_col = X.select_dtypes(include=object).shape[1]
 
 
 encoded_columns = []
@@ -62,67 +63,67 @@ y = pd.get_dummies(y, y[encoded_columns].columns, drop_first=True)
 
 # -- characteristics of datasets
 
-print("number of rows       :", n_rows)
-print("number of columns    :", n_columns)
-print("")
+# print("number of rows       :", n_rows)
+# print("number of columns    :", n_columns)
+# print("")
 
-print("numeric columns      :", n_numeric_col)
-print("non-numeric columns  :", n_non_numeric_col)
-print("")
+# print("numeric columns      :", n_numeric_col)
+# print("non-numeric columns  :", n_non_numeric_col)
+# print("")
 
 
 
 
 # correlation ignoring categorical columns, and only with label (last) column
 
-df2['Class'] = y
+# df2['Class'] = y
 
-corr = df2.corr().abs()
-corr = corr.iloc[: , -1].iloc[:-1]
+# corr = df2.corr().abs()
+# corr = corr.iloc[: , -1].iloc[:-1]
 
-corr_max, corr_mean, corr_min = 0, 0, 0
-if not corr.empty:
-    corr_max = round(corr.max(),3)
-    corr_mean = round(corr.mean(),3)
-    corr_min = round(corr.min(),3)
+# corr_max, corr_mean, corr_min = 0, 0, 0
+# if not corr.empty:
+#     corr_max = round(corr.max(),3)
+#     corr_mean = round(corr.mean(),3)
+#     corr_min = round(corr.min(),3)
     
-print("maximum correlation  :", corr_max)
-print("average correlation  :", corr_mean)
-print("minimum correlation  :", corr_min)
-print("")
+# print("maximum correlation  :", corr_max)
+# print("average correlation  :", corr_mean)
+# print("minimum correlation  :", corr_min)
+# print("")
 
 
 # distinct with only categorical columns, ignoring the label (last) column
 
-print("distinct values in columns:")
-df2 = df.iloc[: , :-1]
+# print("distinct values in columns:")
+# df2 = df.iloc[: , :-1]
+# # for column in df2:
+# #     if df2[column].dtype == object:
+# #         print(column, " - ", df2[column].nunique())
+# # print("")
+
+# list_unique_values = []
 # for column in df2:
 #     if df2[column].dtype == object:
-#         print(column, " - ", df2[column].nunique())
+#         list_unique_values.append(df2[column].nunique())
+
+# mean_unique_values = 0
+# if list_unique_values:
+#     mean_unique_values = Decimal(round(np.mean(list_unique_values),0))
+
+# print("average of distinct values in columns:", mean_unique_values)
 # print("")
 
-list_unique_values = []
-for column in df2:
-    if df2[column].dtype == object:
-        list_unique_values.append(df2[column].nunique())
 
-mean_unique_values = 0
-if list_unique_values:
-    mean_unique_values = Decimal(round(np.mean(list_unique_values),0))
+# imbalance_ratio = 0
+# if y.values.tolist().count([0]) > 0 and y.values.tolist().count([1]) > 0:
+#     if y.values.tolist().count([0]) >= y.values.tolist().count([1]):
+#         imbalance_ratio = round(y.values.tolist().count([0])/y.values.tolist().count([1]),3)
+#     else:
+#         imbalance_ratio = round(y.values.tolist().count([1])/y.values.tolist().count([0]),3)
 
-print("average of distinct values in columns:", mean_unique_values)
-print("")
-
-
-imbalance_ratio = 0
-if y.values.tolist().count([0]) > 0 and y.values.tolist().count([1]) > 0:
-    if y.values.tolist().count([0]) >= y.values.tolist().count([1]):
-        imbalance_ratio = round(y.values.tolist().count([0])/y.values.tolist().count([1]),3)
-    else:
-        imbalance_ratio = round(y.values.tolist().count([1])/y.values.tolist().count([0]),3)
-
-print("imbalance ratio      :", imbalance_ratio)
-print("")
+# print("imbalance ratio      :", imbalance_ratio)
+# print("")
 
 
 
@@ -139,43 +140,47 @@ X, y = smote.fit_resample(X, y)
 
 #   --- k-Fold Cross-Validation ---
 
-# start_time = time.time()
+start_time = time.time()
 
-# algorithm = ExtraTreesClassifier(random_state=42, class_weight='balanced') #.fit(X_train, y_train.values.ravel())
-# #algorithm = KNeighborsClassifier()
+algorithm = ExtraTreesClassifier(random_state=42, class_weight='balanced') #.fit(X_train, y_train.values.ravel())
+#algorithm = KNeighborsClassifier()
 
-# cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=42)
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=42)
 
-# scoring = {'balanced_accuracy': 'balanced_accuracy',
-#            'f1': 'f1', 
-#            'roc_auc': 'roc_auc'}
+scoring = {'balanced_accuracy': 'balanced_accuracy',
+           'f1': 'f1', 
+           'roc_auc': 'roc_auc',
+           'g_mean': make_scorer(geometric_mean_score, greater_is_better=True, average='binary')
+           }
 
-# #, return_train_score=True
-# scores = cross_validate(algorithm, X, y.values.ravel(), scoring=scoring,cv=cv, n_jobs=-1)
-# #10.837
+#, return_train_score=True
+scores = cross_validate(algorithm, X, y.values.ravel(), scoring=scoring,cv=cv, n_jobs=-1)
+#10.837
 
-# finish_time = round(time.time() - start_time,3)
+finish_time = round(time.time() - start_time,3)
 
-# #print("Mean F1 Score        : %.3f" % np.mean(scores_f1))
-# #print("Mean ROC AUC Score   : %.3f" % np.mean(scores_roc_auc))
+#print("Mean F1 Score        : %.3f" % np.mean(scores_f1))
+#print("Mean ROC AUC Score   : %.3f" % np.mean(scores_roc_auc))
 
-# # print("train:")
-# # print("Mean Accuracy Score  : %.3f" % np.mean(scores['train_balanced_accuracy']))
-# # print("Mean F1 Score        : %.3f" % np.mean(scores['train_f1']))
-# # print("Mean ROC AUC         : %.3f" % np.mean(scores['train_roc_auc']))
-# # print("")
-
-# accuracy_score = round(np.mean(scores['test_balanced_accuracy']),3)
-# f1_score = round(np.mean(scores['test_f1']),3)
-# roc_auc_score = round(np.mean(scores['test_roc_auc']),3)
-
-# # print("test:")
-# print("Mean Accuracy Score  :", accuracy_score)
-# print("Mean F1 Score        :", f1_score)
-# print("Mean ROC AUC         :", roc_auc_score)
-
+# print("train:")
+# print("Mean Accuracy Score  : %.3f" % np.mean(scores['train_balanced_accuracy']))
+# print("Mean F1 Score        : %.3f" % np.mean(scores['train_f1']))
+# print("Mean ROC AUC         : %.3f" % np.mean(scores['train_roc_auc']))
 # print("")
-# print("time                 :", finish_time)
+
+accuracy_score = round(np.mean(scores['test_balanced_accuracy']),3)
+f1_score = round(np.mean(scores['test_f1']),3)
+roc_auc_score = round(np.mean(scores['test_roc_auc']),3)
+g_mean_score = round(np.mean(scores['test_g_mean']),3)
+
+# print("test:")
+print("Mean Accuracy Score  :", accuracy_score)
+print("Mean F1 Score        :", f1_score)
+print("Mean ROC AUC         :", roc_auc_score)
+print("Mean G-Mean          :", g_mean_score)
+
+print("")
+print("time                 :", finish_time)
 
 #   --- k-Fold Cross-Validation ---
 
@@ -184,41 +189,41 @@ X, y = smote.fit_resample(X, y)
 
 #   --- train/test ---
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#0.452 sec (23x more fast)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# #0.452 sec (23x more fast)
 
-start_time = time.time()
+# start_time = time.time()
 
-algorithm = ExtraTreesClassifier(random_state=42, class_weight='balanced').fit(X_train, y_train.values.ravel())
+# algorithm = ExtraTreesClassifier(random_state=42, class_weight='balanced').fit(X_train, y_train.values.ravel())
 
-finish_time = round(time.time() - start_time, 3)
+# finish_time = round(time.time() - start_time, 3)
 
-algorithm_pred = algorithm.predict(X_train)
+# algorithm_pred = algorithm.predict(X_train)
 
-#see if overfitting
-# accuracy_score = round(accuracy_score(y_train, algorithm_pred),3)
-# f1_score = round(f1_score(y_train, algorithm_pred),3)
-# roc_auc_score = round(roc_auc_score(y_train, algorithm_pred),3)
+# #see if overfitting
+# # accuracy_score = round(accuracy_score(y_train, algorithm_pred),3)
+# # f1_score = round(f1_score(y_train, algorithm_pred),3)
+# # roc_auc_score = round(roc_auc_score(y_train, algorithm_pred),3)
 
-# print("train")
-# print("metric_accuracy:         ", accuracy_score)
-# print("metric_f1_score          ", f1_score)
-# print("metric_roc_auc_score:    ", roc_auc_score)
+# # print("train")
+# # print("metric_accuracy:         ", accuracy_score)
+# # print("metric_f1_score          ", f1_score)
+# # print("metric_roc_auc_score:    ", roc_auc_score)
 
-algorithm_pred = algorithm.predict(X_test)
+# algorithm_pred = algorithm.predict(X_test)
 
-accuracy_score = round(accuracy_score(y_test, algorithm_pred),3)
-f1_score = round(f1_score(y_test, algorithm_pred),3)
-roc_auc_score = round(roc_auc_score(y_test, algorithm_pred),3)
+# accuracy_score = round(accuracy_score(y_test, algorithm_pred),3)
+# f1_score = round(f1_score(y_test, algorithm_pred),3)
+# roc_auc_score = round(roc_auc_score(y_test, algorithm_pred),3)
 
-#print("test")
-print("metric_accuracy      :", accuracy_score)
-print("metric_f1_score      :", f1_score)
-print("metric_roc_auc_score :", roc_auc_score)
-print("")
+# #print("test")
+# print("metric_accuracy      :", accuracy_score)
+# print("metric_f1_score      :", f1_score)
+# print("metric_roc_auc_score :", roc_auc_score)
+# print("")
 
-print("time                 :", finish_time)
-print("")
+# print("time                 :", finish_time)
+# print("")
 
 #   --- train/test ---
 
