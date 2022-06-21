26dc516179a236fd7923b54f724ce0ae9241bc04
PedroVieira <1160634@isep.ipp.pt>
2022-06-21 20:38:59 +0100

    .

--- project/_test2.py
+++ project/_test2.py
@@ -1,85 +1,31 @@
 import sys
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from ml import read_file, read_file_openml
 
 
 #glass1.dat page-blocks0.dat kddcup-rootkit-imap_vs_back.dat car-good.dat
-dataset_name = "glass1.dat"
+dataset_name = "kddcup-rootkit-imap_vs_back.dat"
 df, dataset_name = read_file(sys.path[0] + "/input/" + dataset_name)
 
 #1069 1056
 # df, dataset_name = read_file_openml(1056)
 
-# print(df)
+print(df)
 
-# print(df["Atr-1"].value_counts())
-# print(df["Atr-2"].value_counts())
-# print(df["Atr-3"].value_counts())
-# print(df["Class"].value_counts())
+X = df.iloc[: , :-1]
+y = df.iloc[: , -1:]
 
-# for each in df.columns:
-#     print(each, df[each].dtype)
+for feature in df.columns:
+    if df[feature].dtype == object or X[feature].dtype.name == 'category' or X[feature].dtype == bool or X[feature].dtype == str:
+        labelencoder = LabelEncoder()
+        df[feature] = labelencoder.fit_transform(df[feature])
+        enc = OneHotEncoder(drop="first", handle_unknown='ignore', sparse=False)
+        df[feature] = enc.fit_transform(df[feature].values.reshape(-1, 1))
+        # enc_df = pd.DataFrame(enc.fit_transform(df[feature].values.reshape(-1, 1))) #.toarray() .values.reshape(-1, 1)
+        # df = df.join(enc_df, how='left', lsuffix='_left', rsuffix='_right')
+        
 
-# print(df[["Class"]])
-
-# preprocessor = OneHotEncoder(drop="first", handle_unknown="ignore")
-
-# df_print = pd.DataFrame(preprocessor.fit_transform(df).toarray()) #.toarray()
-# # pd.set_option('display.max_rows', df_print.shape[0]+1)
-# print(df_print)
-
-
-# X = df.iloc[: , :-1]
-# y = df.iloc[: , -1:]
-
-# print(X)
-# print(y)
-
-# for each in y.columns:
-#     print(each, y[each].dtype == object) #bool
-
-
-# encoded_columns = []
-# for column_name in X.columns:
-#     if X[column_name].dtype == object or X[column_name].dtype.name == 'category' or X[column_name].dtype == bool or X[column_name].dtype == str:
-#         encoded_columns.extend([column_name])
-#         print("coluna alterada:", column_name)
-
-# if encoded_columns:
-#     X = pd.get_dummies(X, columns=X[encoded_columns].columns, drop_first=True) #prefix=encoded_columns
-    
-
-# encoded_columns = []
-# preserve_name = ""
-# for column_name in y.columns:
-#     if y[column_name].dtype == object or y[column_name].dtype.name == 'category' or y[column_name].dtype == bool or y[column_name].dtype == str:
-#         encoded_columns.extend([column_name])
-#         preserve_name = column_name
-#         print("coluna alterada:", column_name)
-
-# # print(y[encoded_columns].columns)
-
-# if encoded_columns:
-#     y = pd.get_dummies(y, columns=y[encoded_columns].columns, drop_first=True)
-
-# if preserve_name:
-#     y.rename(columns={y.columns[0]: preserve_name}, inplace = True)
-
-# print("")
-
-
-
-
-# print(X)
-# print(y)
-
-# for each in X.columns:
-#     print(each, X[each].dtype)
-
-# print("")
-
-# for each in y.columns:
-#     print(each, y[each].dtype)
+print(df)
 
 
