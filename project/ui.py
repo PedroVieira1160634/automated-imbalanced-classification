0db0a957b5edc76d90dcc09194d8b39acd602bf5
PedroVieira <1160634@isep.ipp.pt>
2022-07-03 19:46:05 +0100

    .

--- project/ui.py
+++ project/ui.py
@@ -1,11 +1,12 @@
 import PySimpleGUI as sg
-from ml import execute_ml
+from ml import execute_byCharacteristics
 
 sg.theme('Dark Blue 3')
 
 layout = [[sg.Text('Automated Imbalanced Classification')],
       [sg.Text('Dataset file location ', size=(15, 1)), sg.InputText(), sg.FileBrowse()],
-      [sg.Submit(), sg.Cancel()]]
+      [sg.Submit(), sg.Cancel()],
+      [sg.Text("", size=(0, 1), key='OUTPUT')]]
 
 window = sg.Window('Automated Imbalanced Classification', layout)
 
@@ -15,6 +16,10 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
     
-    execute_ml(values[0], "")
+    print(values[0])
+    window['OUTPUT'].update(value=values[0])
+    
+    # df_dist = execute_byCharacteristics(values[0], "")
+    # window['OUTPUT'].update(value=df_dist)
 
 window.close()
