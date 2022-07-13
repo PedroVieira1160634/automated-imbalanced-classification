d90e213316180f41fb2692b6ca0ff9bb9bc0179e
PedroVieira <1160634@isep.ipp.pt>
2022-07-13 20:54:44 +0100

    .

--- project/_test_openml.py
+++ project/_test_openml.py
@@ -90,73 +90,73 @@ def step_2():
     # print(df_openml.count())
     
     #get X random datasets whithout ...
-    df_openml = df_openml.loc[(df_openml["id"] != 1069) &   #bad metrics
-                              (df_openml["id"] != 1506) & 
-                              (df_openml["id"] != 1452) & 
-                              (df_openml["id"] != 43905) & 
+    # df_openml = df_openml.loc[(df_openml["id"] != 1069) &   #bad metrics
+    #                           (df_openml["id"] != 1506) & 
+    #                           (df_openml["id"] != 1452) & 
+    #                           (df_openml["id"] != 43905) & 
                               
-                              (df_openml["id"] != 1056) &   #too long
+    #                           (df_openml["id"] != 1056) &   #too long
                               
-                              (df_openml["id"] != 43897) &  #perfect metrics
-                              (df_openml["id"] != 43895) & 
-                              (df_openml["id"] != 40666) &
+    #                           (df_openml["id"] != 43897) &  #perfect metrics
+    #                           (df_openml["id"] != 43895) & 
+    #                           (df_openml["id"] != 40666) &
                               
-                              (df_openml["id"] != 951) &    #testing UI
-                              (df_openml["id"] != 43051) &
-                              (df_openml["id"] != 1451) &
-                              (df_openml["id"] != 40713) &
-                              (df_openml["id"] != 1487) &
-                              (df_openml["id"] != 1116) &
-                              (df_openml["id"] != 41538) &
-                              (df_openml["id"] != 4329) &
-                              (df_openml["id"] != 1447) &
-                              (df_openml["id"] != 949) &
+    #                           (df_openml["id"] != 951) &    #testing UI
+    #                           (df_openml["id"] != 43051) &
+    #                           (df_openml["id"] != 1451) &
+    #                           (df_openml["id"] != 40713) &
+    #                           (df_openml["id"] != 1487) &
+    #                           (df_openml["id"] != 1116) &
+    #                           (df_openml["id"] != 41538) &
+    #                           (df_openml["id"] != 4329) &
+    #                           (df_openml["id"] != 1447) &
+    #                           (df_openml["id"] != 949) &
                               
-                              (df_openml["id"] != 1069) &   #already trainned
-                              (df_openml["id"] != 40994) &
-                              (df_openml["id"] != 450) &
-                              (df_openml["id"] != 41946) &
-                              (df_openml["id"] != 1020) &
-                              (df_openml["id"] != 1506) &
-                              (df_openml["id"] != 765) &
-                              (df_openml["id"] != 43894) &
-                              (df_openml["id"] != 40900) &
-                              (df_openml["id"] != 1452) &
-                              (df_openml["id"] != 1558) &
-                              (df_openml["id"] != 1467) &
-                              (df_openml["id"] != 995) &
-                              (df_openml["id"] != 43905) &
-                              (df_openml["id"] != 958) &
-                              (df_openml["id"] != 1065) &
-                              (df_openml["id"] != 1067) &
-                              (df_openml["id"] != 764) &
-                              (df_openml["id"] != 311) &
-                              (df_openml["id"] != 1068) &
-                              (df_openml["id"] != 43893) &
-                              (df_openml["id"] != 1022) &
-                              (df_openml["id"] != 976) &
-                              (df_openml["id"] != 1444) &
-                              (df_openml["id"] != 954) &
-                              (df_openml["id"] != 1050) &
-                              (df_openml["id"] != 1446) &
-                              (df_openml["id"] != 767) &
-                              (df_openml["id"] != 1049) &
-                              (df_openml["id"] != 980)
-                              ]
+    #                           (df_openml["id"] != 1069) &   #already trainned
+    #                           (df_openml["id"] != 40994) &
+    #                           (df_openml["id"] != 450) &
+    #                           (df_openml["id"] != 41946) &
+    #                           (df_openml["id"] != 1020) &
+    #                           (df_openml["id"] != 1506) &
+    #                           (df_openml["id"] != 765) &
+    #                           (df_openml["id"] != 43894) &
+    #                           (df_openml["id"] != 40900) &
+    #                           (df_openml["id"] != 1452) &
+    #                           (df_openml["id"] != 1558) &
+    #                           (df_openml["id"] != 1467) &
+    #                           (df_openml["id"] != 995) &
+    #                           (df_openml["id"] != 43905) &
+    #                           (df_openml["id"] != 958) &
+    #                           (df_openml["id"] != 1065) &
+    #                           (df_openml["id"] != 1067) &
+    #                           (df_openml["id"] != 764) &
+    #                           (df_openml["id"] != 311) &
+    #                           (df_openml["id"] != 1068) &
+    #                           (df_openml["id"] != 43893) &
+    #                           (df_openml["id"] != 1022) &
+    #                           (df_openml["id"] != 976) &
+    #                           (df_openml["id"] != 1444) &
+    #                           (df_openml["id"] != 954) &
+    #                           (df_openml["id"] != 1050) &
+    #                           (df_openml["id"] != 1446) &
+    #                           (df_openml["id"] != 767) &
+    #                           (df_openml["id"] != 1049) &
+    #                           (df_openml["id"] != 980)
+    #                           ]
     
-    # print(df_openml.sample(n=10))
-    print(df_openml)
+    # print(df_openml)
+    print(df_openml.sample(n=10))
     
 def step_3():
-    df, dataset_name = read_file_openml(40910)
+    df, dataset_name = read_file_openml(951)
 
-    print("\ndf:")
-    print(df)
+    # print("\ndf:")
+    # print(df)
 
     print("\ndataset_name:")
     print(dataset_name)
 
 
 # step_1()
-step_2()
-# step_3()
+# step_2()
+step_3()
--- project/_test_ui.py
+++ project/_test_ui.py
@@ -1,4 +1,6 @@
 import sys
+import time
+import datetime
 import pandas as pd
 import numpy as np
 from ml import get_best_results_by_characteristics, execute_byCharacteristics, write_characteristics_remove_current_dataset, display_final_results
@@ -23,8 +25,26 @@ from ml import get_best_results_by_characteristics, execute_byCharacteristics, w
 # str_output = execute_byCharacteristics(sys.path[0] + "/input/" + dataset_name, "")
 # print(str_output)
 
-#951,43051,1451,40713,1487,1116,41538,4329,1447,949
 
-str_output = execute_byCharacteristics("", 951)
+#41538   0:01       conference_attendance
+#4329    0:01       thoracic_surgery
+#1447    0:01       CastMetal1
+#949     0:01       arsenic-female-bladder
+#951     0:02       arsenic-male-lung
+#1451    0:02       PieChart1 
+#1487    0:18       ozone-level-8hr
+#40713   0:24       dis
+#1116    4:13       musk
+#43051   too long?  art_daily_nojump4
+
+#see also results
+
+start_time = time.time()
+
+str_output = execute_byCharacteristics("", 949)
 print("\nFinal UI Table:\n", str_output)
+
+finish_time = (round(time.time() - start_time,3))
+finish_time = str(datetime.timedelta(seconds=round(finish_time,0)))
+print("Elapsed Time                 :", finish_time, "\n")
 
