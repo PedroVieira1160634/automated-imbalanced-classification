9a38f5d8e3176831585c9c40f78cdd9ecb0d5d62
PedroVieira <1160634@isep.ipp.pt>
2022-12-19 19:49:36 +0000

    updated test_ml

--- project/test_ml.py
+++ project/test_ml.py
@@ -5,7 +5,7 @@ from ml import execute_ml, execute_ml_test
 print('\n\n----------------------------------start -', datetime.now(), '--------------------------------------')
 
 #FILE
-dataset_name = "zoo-3.dat"
+dataset_name = "car-good.dat"
 
 # execute_ml_test(sys.path[0] + "/input/" + dataset_name, "")
 execute_ml(sys.path[0] + "/input/" + dataset_name, "")
@@ -14,14 +14,14 @@ execute_ml(sys.path[0] + "/input/" + dataset_name, "")
 
 #OPENML
 #testing
-# execute_ml_test("", 975)
+# execute_ml_test("", 765)
 
 # list_openml = [986, 43595, 1010, 897, 13, 990]
 # for id in list_openml:
 #     execute_ml_test("", id)
 #     print("\n----------------------------------\n")
 
-# execute_ml("", 975)
+# execute_ml("", 765)
 
 # list_openml = [986, 43595, 1010, 897, 13, 990]
 # for id in list_openml:
