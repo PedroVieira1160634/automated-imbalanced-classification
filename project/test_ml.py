497ebb7bd9e8165c767c3a034c48ceeff64b510e
PedroVieira <1160634@isep.ipp.pt>
2024-01-24 20:54:08 +0000

    execution of 2 datasets of files

--- project/test_ml.py
+++ project/test_ml.py
@@ -6,38 +6,38 @@ print('\n\n----------------------------------start -', datetime.now(), '--------
 
 # FILE
 
-# dataset_name = "car-good.dat"
-# execute_ml_test(sys.path[0] + "/input/" + dataset_name, "")
-# execute_ml(sys.path[0] + "/input/" + dataset_name, "")
+# # dataset_name = "car-good.dat"
+# # execute_ml_test(sys.path[0] + "/input/" + dataset_name, "")
+# # execute_ml(sys.path[0] + "/input/" + dataset_name, "")
 
 # list_files = ["poker-8-9_vs_6.dat","kr-vs-k-zero-one_vs_draw.dat","kr-vs-k-zero_vs_eight.dat","kr-vs-k-three_vs_eleven.dat","dermatology-6.dat",
 #                "car-vgood.dat","abalone-21_vs_8.dat","kr-vs-k-zero_vs_fifteen.dat","poker-8_vs_6.dat","car-good.dat","page-blocks0.dat","glass1.dat"]
-list_files = ["page-blocks0.dat","glass1.dat"]
-for dataset_name in list_files:
-    execute_ml(sys.path[0] + "/input/" + dataset_name, "")
-    print("\n----------------------------------\n")
+# for dataset_name in list_files:
+#     execute_ml(sys.path[0] + "/input/" + dataset_name, "")
+#     print("\n----------------------------------\n")
 
 #FILE
 
 
 # #OPENML
 
-# # execute_ml_test("", 986)
-# # execute_ml("", 986)
+# execute_ml_test("", 986)
+# execute_ml("", 986)
 
-# # list_openml = [986, 43595, 1010, 897, 13, 990]
-# # for id in list_openml:
-# #     execute_ml_test("", id)
-# #     print("\n----------------------------------\n")
+# list_openml = [986, 43595, 1010, 897, 13, 990]
+# for id in list_openml:
+#     execute_ml_test("", id)
+#     print("\n----------------------------------\n")
 
-# # execute_ml("", 765)
+# execute_ml("", 765)
 
 # list_openml = [990, 897, 1010, 975, 950, 980, 767, 311, 958, 995, 40900, 765, 40994, 42172, 1003, 1037, 966, 1023, 968, 42883, 833, 761, 735, 1489,
 #                1524, 1511, 312, 1004, 934, 1025, 728, 41156, 796, 994, 733, 337, 40701, 978, 971, 947, 40983, 1016, 962, 1049, 954, 976, 1022, 764,
 #                1020, 41946, 450, 1116, 40713]
-# for id in list_openml:
-#     execute_ml("", id)
-#     print("\n----------------------------------\n")
+list_openml = [990, 897, 1010, 975, 950]
+for id in list_openml:
+    execute_ml("", id)
+    print("\n----------------------------------\n")
 # #OPENML
 
 print('----------------------------------finish -', datetime.now(), '--------------------------------------\n\n')
